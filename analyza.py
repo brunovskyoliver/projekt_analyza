c7e645f71a8b1aaa415e19a63d9e3e7362ce21a5
janStevo <jankovicstevo1@gmail.com>
2024-10-23 20:10:41 +0200

    prve_dve_sifry

--- analyza.py
+++ analyza.py
@@ -1,7 +1,71 @@
 veta=input("Zadaj vetu: ")
-pocet_slov=1
+
+###### !!! cvicna veta !!! #######
+# veta = "Katka a Zdenka su sestry."
+
+# konštanty
+    # znaky a písmená
 samohlasky="aeiouyAEIOUY"
 spoluhlasky="bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ"
+intrepZnamienka = ".?!"
+    # ASCII kód
+prveMalePismeno = 97 # hodnota malého a v ASCII kóde
+rozdielHodnotPismen = 32 # rozdiel medzi hodnotou veľkého a malého pismena
+pismenAbecedy = 26 # počet pśsmen anglickej abecedy
+
+def vypocitajSifra1(iVeta):
+    lSifra = ""
+    lPoslednaMedzera: int = 0
+    for poz in range(len(iVeta)):
+        pismeno = iVeta[poz]
+        if pismeno == " " or pismeno in intrepZnamienka:
+            lSifra += " " + iVeta[(poz-1):(lPoslednaMedzera):-1] if lPoslednaMedzera != 0 else iVeta[(poz-1)::-1]
+            lPoslednaMedzera = poz
+    lNoveSlovo = ""
+    for pismeno in lSifra:
+        if ord(pismeno) >= prveMalePismeno:
+            lNoveSlovo += chr(ord(pismeno) - rozdielHodnotPismen)
+        else:
+            lNoveSlovo += pismeno
+    lSifra = lNoveSlovo
+    return lSifra
+
+def vypocitajSifra2(iVeta):
+    lPosun: int = 1
+    lSifra = ""
+    for pismeno in iVeta:
+        lPosun = lPosun%pismenAbecedy
+        lPoradiePismena = ord(pismeno)
+        if pismeno == " " or pismeno in intrepZnamienka:
+            lPosun = 1
+            lSifra += pismeno
+        else:
+            if (lPoradiePismena + lPosun >= prveMalePismeno and lPoradiePismena + lPosun <= prveMalePismeno + (pismenAbecedy-1)) \
+                or (lPoradiePismena + lPosun >= prveMalePismeno - rozdielHodnotPismen and lPoradiePismena + lPosun <= prveMalePismeno - rozdielHodnotPismen + (pismenAbecedy-1)):
+            # zisťujeme či sa novo vzniknuté písmeno nachádza medzi veľkými alebo malými písmenami ASCII kódu
+                lSifra += chr(lPoradiePismena + lPosun)
+            else:
+                lSifra += chr(lPoradiePismena + lPosun - pismenAbecedy)
+            lPosun += 1
+    return lSifra
+
+def vypocitajSifra3(iVeta):
+    return ""
+
+def komprimuj(iVeta):
+    return ""
+
+def sifry(iVeta):
+    sifra1 = vypocitajSifra1(iVeta=iVeta)
+    sifra2 = vypocitajSifra2(iVeta=iVeta)
+    sifra3 = vypocitajSifra3(iVeta=iVeta)
+    komprimVeta = komprimuj(iVeta=iVeta)
+    print("Šifra 1: " + sifra1)
+    print("Šifra 2: " + sifra2)
+    print("Šifra 3: " + sifra3)
+    print("Kompresia: " + komprimVeta)
+
+pocet_slov=1
 slova_na_samohlasku=0
 slova_na_spoluhlasku=0
 
@@ -21,8 +85,8 @@ for a in range(len(veta)):
 print("Počet slov vo vete: "+str(pocet_slov))
 print("Počet slov začínajúcich na samohlásku: "+str(slova_na_samohlasku))
 print("Počet slov začínajúcich na spoluhlásku: "+str(slova_na_spoluhlasku))
-print("Dĺžky slov vo vete:") # tst01
-#test02
+print("Dĺžky slov vo vete:")
+
 posledna_medzera=-1
 sucasna_medzera=0
 dlzka_max_slova=0
@@ -31,7 +95,10 @@ count_slovo = 0
 najdlhsie_slova = ""
 
 for a in range(len(veta)):
-    if veta[a]==" " or veta[a] in ".?!":
+    # <--2024-10-23 - SJ - nova konstanta
+    # if veta[a]==" " or veta[a] in ".?!":
+    if veta[a]==" " or veta[a] in intrepZnamienka:
+    # SJ-->
         sucasna_medzera=a
         count_slovo +=1
         print(veta[(posledna_medzera+1):sucasna_medzera]+" - "+str(sucasna_medzera-(posledna_medzera+1)))
@@ -42,7 +109,7 @@ for a in range(len(veta)):
         if len(veta[(posledna_medzera+1):sucasna_medzera]) == dlzka_max_slova:
             najdlhsie_slova += veta[(posledna_medzera+1):sucasna_medzera]
         posledna_medzera=sucasna_medzera
-    elif a == (len(veta)-1):
+    elif a == (len(veta)-1): # SJ - spravna veta ma na konci ".?!", preto tento elif nebude nikdy vyuzity
         sucasna_medzera=a+1
         print(veta[(posledna_medzera+1):sucasna_medzera]+" - "+str(sucasna_medzera-(posledna_medzera+1)))
         if (sucasna_medzera-(posledna_medzera+1))>dlzka_max_slova:
@@ -60,3 +127,4 @@ print("Slová s najväčšou dĺžkou: ")
 for i in range(int(len(najdlhsie_slova)/dlzka_max_slova)):
     print(najdlhsie_slova[i*dlzka_max_slova:(i*dlzka_max_slova+dlzka_max_slova)])
 
+sifry(iVeta=veta)
